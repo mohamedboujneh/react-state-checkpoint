b90d042f371e6f42c94b5c8a4acadffbdd170400
mohamed boujneh <mohamedboujneh@hotmail.com>
2021-08-11 14:27:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+
+constructor(props){
+  super(props);
+
+  this.state={
+    isToggleOn:false,
+    Person :{ fullName : 'Mohamed Boujneh',
+    bio : 'Life is way too short for bad vibes.', 
+    imgSrc: logo,
+    profession:'Ingénieur informatique'} ,
+   }
+   this.handleClick= this.handleClick.bind(this);
+
+}
+
+ 
+
+handleClick(){
+  this.setState(state=>({
+    isToggleOn : !state.isToggleOn
+  }))
+}
+
+ 
+  render (){
+   
+   return(<div  >
+      <button onClick={this.handleClick} >Afficher </button>
+       <br/>
+       {  this.state.isToggleOn ?  <h1 > Fullname:  {this.state.Person.fullName}</h1> :''}
+        {  this.state.isToggleOn ? <h1> Bio:  {this.state.Person.bio}</h1>:''}
+        {  this.state.isToggleOn ?  <h1> Profession: { this.state.Person.profession}</h1>:''}
+      
+        {  this.state.isToggleOn ?    <img  src={this.state.Person.imgSrc}/> :''} 
+
+
+         
     </div>
-  );
+   );
+  };
+
+
+
 }
 
 export default App;
